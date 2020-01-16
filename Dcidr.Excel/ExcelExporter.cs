c4bb1ba4ad32126c1689d0480ca35a16f950a092
Tim Plourde <tplourde@gmail.com>
2020-01-15 22:05:26 -0500

    finished excel export

--- Dcidr.Excel/ExcelExporter.cs
+++ Dcidr.Excel/ExcelExporter.cs
@@ -1,6 +1,7 @@
 ﻿using Dcidr.Model;
 using OfficeOpenXml;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Dcidr.Excel
@@ -16,19 +17,146 @@ namespace Dcidr.Excel
 
             using (var package = new ExcelPackage())
             {
-                ExcelWorksheet criteriaComparisons = package.Workbook.Worksheets.Add("CriteriaComparisons");
-                BuildCriteriaComparisonSheet(criteriaComparisons, decision);
+                var criteriaRdvDictionary = BuildCriteriaComparisonSheet(package, decision);
+                var optionRdvDictionary = new Dictionary<string, Dictionary<string, string>>();
+                foreach(var criterion in decision.Criteria.Items)
+                {
+                    optionRdvDictionary.Add(criterion, BuildOptionComparisonSheet(package, decision, criterion));
+                }
+                BuildSymmarySheet(package, decision, criteriaRdvDictionary, optionRdvDictionary);
                 return package.GetAsByteArray();
             }
         }
 
-        private static void BuildCriteriaComparisonSheet(ExcelWorksheet sheet, Decision decision)
+        private static void BuildSymmarySheet(ExcelPackage excelPackage, Decision decision, Dictionary<string,string> criteriaRdvDictionary, Dictionary<string, Dictionary<string,string>> optionRdvDictionary)
         {
+            var sheet = excelPackage.Workbook.Worksheets.Add("Summary");
+
+            var totalCriteria = decision.Criteria.Items.Count();
+            var totalOptions = decision.Options.Items.Count();
+
+            // criteria column labels
+            for (int i = 0; i < totalCriteria; i++)
+            {
+                sheet.Cells[1, 2 + i].Value = decision.Criteria.Items.ElementAt(i);
+            }
+
+            // grand total column label
+            sheet.Cells[1, totalCriteria + 2].Value = "Weighted Relative Decimal Value";
+
+            for (int i = 0; i < totalOptions; i++)
+            {
+                // option row labels
+                var option = decision.Options.Items.ElementAt(i);
+                sheet.Cells[2 + i, 1].Value = option;
+                for (int j = 0; j < totalCriteria; j++)
+                {
+                    var criterion = decision.Criteria.Items.ElementAt(j);
+                    var criteriaRdvCellAddress = criteriaRdvDictionary[criterion];
+                    var optionRdvCellAddress = optionRdvDictionary[criterion][option];
+                    sheet.Cells[2 + i, 2 + j].Formula = $"={criteriaRdvCellAddress}*{optionRdvCellAddress}";
+                }
+                
+                // grand total column
+                var grandTotalRange = new ExcelAddress(2 + i, 2,
+                    2 + i, 1 + totalCriteria);
+                var grandTotalCellAddress = new ExcelCellAddress(2 + i, 2 + totalCriteria);
+                sheet.Cells[grandTotalCellAddress.Address].Formula = $"=SUM({grandTotalRange})";
+                sheet.Cells[grandTotalCellAddress.Address].Style.Font.Bold = true;
+            }
+
+            //this throws a PlatformNotSupportedException becuase of a call to GDI
+            //sheet.Cells.AutoFitColumns();
+
+            excelPackage.Workbook.Worksheets.MoveToStart("Summary");
+        }
+
+        /// <summary>
+        /// Adds an option comparison sheet (for a criterion) to the package
+        /// </summary>
+        /// <param name="excelPackage"></param>
+        /// <param name="decision"></param>
+        /// <param name="criterion"></param>
+        /// <returns>dictionary or RDV cell addresses one for each option</returns>
+        private static Dictionary<string,string> BuildOptionComparisonSheet(ExcelPackage excelPackage, Decision decision, string criterion)
+        {
+            var optionRdvCellAddresses = new Dictionary<string, string>();
+
+            var sheet = excelPackage.Workbook.Worksheets.Add(criterion);
+            var totalOptions = decision.Options.Items.Count();
+            sheet.Cells[1, 2 + totalOptions].Value = "Total";
+            sheet.Cells[1, 3 + totalOptions].Value = "Relative Decimal Value";
+
+            // grand total formula
+            var grandTotalRange = new ExcelAddress(2, totalOptions + 2,
+                totalOptions + 1, totalOptions + 2);
+            var grandTotalCellAddress = new ExcelCellAddress(totalOptions + 2, totalOptions + 2);
+            sheet.Cells[grandTotalCellAddress.Address].Formula = $"SUM({grandTotalRange})";
+
+            for (int i = 0; i < totalOptions; i++)
+            {
+                var option = decision.Options.Items.ElementAt(i);
+
+                // column labels
+                sheet.Cells[1, 2 + i].Value = option;
+
+                // row labels
+                sheet.Cells[2 + i, 1].Value = option;
+
+                // total column formula
+                var totalRange = new ExcelAddress(2 + i, 2, 2 + i, 1 + totalOptions);
+                var totalCellAddress = new ExcelCellAddress(2 + i, 2 + totalOptions);
+                sheet.Cells[totalCellAddress.Address].Formula = $"SUM({totalRange})";
+
+                // RDV column formula
+                var optionRdvCellAddress = new ExcelCellAddress(2 + i, 3 + totalOptions);
+                optionRdvCellAddresses.Add(option, $"{criterion}!{optionRdvCellAddress.Address}");
+                sheet.Cells[optionRdvCellAddress.Address].Formula = $"{totalCellAddress.Address}/{grandTotalCellAddress.Address}";
+            }
+
+            // weights and inverses
+            for (int i = 0; i < totalOptions; i++)
+            {
+                for (int j = 0; j < totalOptions; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    var optionOne = decision.Options.Items.ElementAt(i);
+                    var optionTwo = decision.Options.Items.ElementAt(j);
+
+                    var optionComparison = decision.OptionComparisons.FirstOrDefault(cc => cc.OptionOne == optionOne 
+                        && cc.OptionTwo == optionTwo
+                        && cc.Criterion == criterion);
+
+                    if (optionComparison != null)
+                    {
+                        sheet.Cells[i + 2, j + 2].Value = optionComparison.Weight.Value.ToRatio();
+                        sheet.Cells[j + 2, i + 2].Value = 1 / optionComparison.Weight.Value.ToRatio();
+                    }
+                }
+            }
+            return optionRdvCellAddresses;
+        }
+
+        /// <summary>
+        /// Adds the criteria comparison sheet
+        /// </summary>
+        /// <param name="excelPackage"></param>
+        /// <param name="decision"></param>
+        /// <returns>Dictinary of cell addresses of criteria RDV values</returns>
+        private static Dictionary<string, string> BuildCriteriaComparisonSheet(ExcelPackage excelPackage, Decision decision)
+        {
+            const string sheetTitle = "CriteriaComparisons";
+            var criteriaRdvCellAddresses = new Dictionary<string, string>();
+
+            var sheet = excelPackage.Workbook.Worksheets.Add(sheetTitle);
             var totalCriteria = decision.Criteria.Items.Count();
             sheet.Cells[1, 2 + totalCriteria].Value = "Total";
             sheet.Cells[1, 3 + totalCriteria].Value = "Relative Decimal Value";
 
-            // grand total formuma
+            // grand total formula
             var grandTotalRange = new ExcelAddress(2, totalCriteria + 2,
                 totalCriteria + 1, totalCriteria + 2);
             var grandTotalCellAddress = new ExcelCellAddress(totalCriteria + 2, totalCriteria + 2);
@@ -50,7 +178,9 @@ namespace Dcidr.Excel
                 sheet.Cells[totalCellAddress.Address].Formula = $"SUM({totalRange})";
 
                 // RDV column formula
-                sheet.Cells[2 + i, 3 + totalCriteria].Formula = $"{totalCellAddress.Address}/{grandTotalCellAddress.Address}";
+                var criteriaRdvAddress = new ExcelCellAddress(2 + i, 3 + totalCriteria);
+                criteriaRdvCellAddresses.Add(crit, $"{sheetTitle}!{criteriaRdvAddress.Address}");
+                sheet.Cells[criteriaRdvAddress.Address].Formula = $"{totalCellAddress.Address}/{grandTotalCellAddress.Address}";
             }
 
             // weights and inverses
@@ -73,6 +203,7 @@ namespace Dcidr.Excel
                     }
                 }
             }
+            return criteriaRdvCellAddresses;
         }
     }
 }
