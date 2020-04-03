b869616de4bf36c0ee1140eb17bcb02bb298ec73
Tim Plourde <tplourde@gmail.com>
2020-04-03 16:59:31 -0400

    updated PWA to use new manifest generator

--- ship.ps1
+++ ship.ps1
@@ -3,9 +3,5 @@ dotnet publish -c Release
 
 $publishPath = '.\Dcidr.Blazor\bin\Release\netstandard2.1\publish\wwwroot';
 
-# bump sw cache version number
-$serviceWOrkerPath = $publishPath + '\service-worker.js';
-((Get-Content -path $serviceWOrkerPath -Raw) -replace 'VERSION', [guid]::NewGuid().ToString()) | Set-Content -Path $serviceWOrkerPath
-
 # this requires azcopy to be logged in
 azcopy sync $publishPath "https://dcidr.blob.core.windows.net/`$web" --delete-destination true
