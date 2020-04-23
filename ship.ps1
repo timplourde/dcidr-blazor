eda5383739b7923f31e70c6e083c801923fe33c9
Tim Plourde <tplourde@gmail.com>
2020-04-22 21:13:56 -0400

    remove ship.ps1

--- ship.ps1
+++
@@ -1,7 +0,0 @@
-# publish app
-dotnet publish -c Release
-
-$publishPath = '.\Dcidr.Blazor\bin\Release\netstandard2.1\publish\wwwroot';
-
-# this requires azcopy to be logged in
-azcopy sync $publishPath "https://dcidr.blob.core.windows.net/`$web" --delete-destination true
